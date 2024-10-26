d2a4c9725c1de6ea7fa07f91fda90b35a974d788
dalie <dalie@10.5.17.99>
2024-10-26 12:23:52 +0200

    T

--- OOP/src/main/Main.java
+++ OOP/src/main/Main.java
@@ -10,6 +10,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		new FreizeitbaederAnwendersystem(primaryStage);
+		//h
 	}	
 	
 	public static void main(String[] args){
